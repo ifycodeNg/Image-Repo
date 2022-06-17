7121df8e94cfd71d579cdd8fb2fd356bdc6d7349
ifycode <ifyugwumba22@gmail.com>
2022-06-17 18:44:54 +0100

    First Commit

---
+++ src/server.ts
@@ -0,0 +1,34 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import {filterImageFromURL, deleteLocalFiles} from './util/util';
+
+(async () => {
+
+  // Init the Express application
+  const app = express();
+
+  // Set the network port
+  const port = process.env.PORT || 8082;
+  
+  // Use the body parser middleware for post requests
+  app.use(bodyParser.json());
+
+  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
+  // GET /filteredimage?image_url={{URL}}
+  app.get( "/filteredimage", async ( req, res ) => {
+    let image_url   = (req.query['image_url']) as string
+    const filerImage =  filterImageFromURL(image_url)
+    res.status(201).sendFile(await filerImage)
+  } );
+  
+ app.get( "/", async ( req, res ) => {
+    res.send("try GET /filteredimage?image_url={{}}")
+  } );
+  
+
+  // Start the Server
+  app.listen( port, () => {
+      console.log( `server running http://localhost:${ port }` );
+      console.log( `press CTRL+C to stop server` );
+  } );
+})();
