0542e2f5dec1878ffc15b94d6724ef307b7b7caf
ifycode <ifyugwumba22@gmail.com>
2022-06-19 10:04:17 +0100

    Dev Update

---
+++ src/secret/config.ts
@@ -0,0 +1,5 @@
+export={
+    secret:{ 
+        "Access_Token":process.env.ACCESS_TOKEN_SECRET || "1bfa4f9eb9a863f875af0f142aa7347e6f0e104c906efb0801be94c5f9ba9db2"
+}
+}
--- src/server.ts
+++ src/server.ts
@@ -12,16 +12,19 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
-
+  
   // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
   // GET /filteredimage?image_url={{URL}}
-  app.get( "/filteredimage", async ( req, res ) => {
+  app.get( "/filteredimage", async ( req:express.Request, res :express.Response ) => {
     let image_url   = (req.query['image_url']) as string
+    if(!image_url){
+     return res.status(422).json({msg:"Please Input Image Url"})
+    }
     const filerImage =  filterImageFromURL(image_url)
-    res.status(201).sendFile(await filerImage)
+    res.status(200).sendFile(await filerImage)
   } );
   
- app.get( "/", async ( req, res ) => {
+ app.get( "/", async ( req:express.Request, res :express.Response ) => {
     res.send("try GET /filteredimage?image_url={{}}")
   } );
   
